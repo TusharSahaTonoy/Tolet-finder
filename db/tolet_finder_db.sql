dd1618c248acdef851c23e455ae02c266578fd00
TusharSahaTonoy <tonoysh@gmail.com>
2019-03-18 19:23:46 +0600

    sql file changed

--- db/tolet_finder_db.sql
+++ db/tolet_finder_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 03, 2018 at 04:10 PM
+-- Generation Time: Mar 18, 2019 at 02:17 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -25,6 +25,37 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `counter` int(11) NOT NULL,
+  `id` varchar(50) NOT NULL,
+  `title` varchar(300) NOT NULL,
+  `description` text NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_email` varchar(50) NOT NULL,
+  `preferred_client` varchar(50) NOT NULL,
+  `month` varchar(50) NOT NULL,
+  `offering` varchar(50) NOT NULL,
+  `quantity` int(50) NOT NULL,
+  `sublet` varchar(50) NOT NULL,
+  `district` varchar(50) NOT NULL,
+  `aria` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `price` double NOT NULL,
+  `nagotiable` varchar(50) NOT NULL,
+  `image1` varchar(350) NOT NULL,
+  `image2` varchar(350) DEFAULT NULL,
+  `image3` varchar(350) DEFAULT NULL,
+  `approved` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
@@ -35,14 +66,25 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Indexes for dumped tables
 --
 
-INSERT INTO `user` (`name`, `email`, `password`) VALUES
-('Test Name', 'test@email', '1234'),
-('Tushar', 'test@email.com', '1234'),
-('name1', 'qas@gs.mn', '1234'),
-('Tushar', 'tushar@email.com', '1234');
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `counter` (`counter`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `counter` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
